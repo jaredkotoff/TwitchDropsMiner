3744769651b3969ab5c2e73357e87493c13fda48
Valentin Metz <Valentin.Metz@web.de>
2023-02-28 08:26:34 +0100

    Remove display lock on container exit

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -4,6 +4,7 @@ printf '%(%Y-%m-%d %H:%M:%S)T: Started new container\n' -1
 
 # Start X virtual framebuffer
 export DISPLAY=:1
+rm /tmp/.X1-lock -f
 Xvfb :1 -screen 0 640x480x8 -nolisten tcp &
 
 # Execute CMDs
