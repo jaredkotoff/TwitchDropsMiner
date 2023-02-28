ef705644b6f1ebd0ffd61f21fbfd9e4c44749fd0
Valentin Metz <Valentin.Metz@web.de>
2023-02-28 03:48:08 +0100

    Print date on container stop

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -5,4 +5,8 @@ printf '%(%Y-%m-%d %H:%M:%S)T: Started new container\n' -1
 # Start X virtual framebuffer
 export DISPLAY=:1
 Xvfb :1 -screen 0 640x480x8 -nolisten tcp &
-exec "$@"
+
+# Execute CMDs
+"$@"
+
+printf '%(%Y-%m-%d %H:%M:%S)T: Stopped container\n' -1
