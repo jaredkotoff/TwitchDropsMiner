8e5971685e558ddd8f7be2b852801bc59f80359a
Valentin Metz <Valentin.Metz@web.de>
2024-03-08 20:55:42 +0100

    Decreased max sleep period with no campaigns active

--- twitch.py
+++ twitch.py
@@ -1123,7 +1123,7 @@ class Twitch:
     @task_wrapper
     async def _maintenance_task(self) -> None:
         claim_period = timedelta(minutes=30)
-        max_period = timedelta(hours=1)
+        max_period = timedelta(minutes=5)
         now = datetime.now(timezone.utc)
         next_period = now + max_period
         while True:
